b2c121081f3340829e5edaa0dc3ab37a88368f77
Jackaboi18 <38188828+Jackaboi18@users.noreply.github.com>
2018-06-01 17:22:14 +0100

    Update and rename .gitignore to sao.py

---
+++ sao.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+import os
+Client = discord.Client()
+client = commands.Bot(command_prefix = "`")
+client.remove_command('help')
+@client.event
+async def on_ready():
+    print("Bot is online and connected to Discord")
+    await client.change_presence(game=discord.Game(name="SAO Coming Soon!"))
+@client.command(pass_context=True)
+async def ip(ctx):
+    embed=discord.Embed(title="SAO", description="SAO PE Coming Soon!", color=0xFCFF0F)
+    await client.say(embed=embed)
+ 
+client.run(os.getenv('TOKEN'))
